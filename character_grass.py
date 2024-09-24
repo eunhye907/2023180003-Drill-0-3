209bd33707f1d9d771319a6dad7ca7705aeacfa4
Test <gracekwak907@naver.com>
2024-09-24 11:57:01 +0900

    캐릭터 화면 출력 구현

--- character_grass.py
+++ character_grass.py
@@ -7,15 +7,22 @@ boy = load_image('character.png')
 
 
 def run_circle():
+	print('CIRCLE')
+	
+	clear_canvas_now()
+	boy.draw_now(400,300)
+	delay(0.1)
+
 	pass
 
 def run_rectangle():
+	print('RECTANGLE')
 	pass
 
 while True:
 	run_circle()
 	run_rectangle()
-
+	break
 
 
 close_canvas()
