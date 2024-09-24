aef465dd8672e994131963515cf901292684d8c4
Test <gracekwak907@naver.com>
2024-09-24 11:49:55 +0900

    뼈대 완성 (프린트로 무한루프 구조 확인)

---
+++ character_grass.py
@@ -0,0 +1,21 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+
+def run_circle():
+	pass
+
+def run_rectangle():
+	pass
+
+while True:
+	run_circle()
+	run_rectangle()
+
+
+
+close_canvas()
